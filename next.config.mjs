3bb8c92e88ed42c6f7c58276183267efdfd3fcfa
2hg7trp7rv-design <2hg7trp7rv@privaterelay.appleid.com>
2025-12-01 04:03:07 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // React の厳格モード
   reactStrictMode: true,
-  // Cloudflare Pages で静的サイトとして配信する
+
+  // Cloudflare Pages で配信するための静的エクスポート
   output: "export",
+
+  // 静的ホスティング用に、next/image の最適化を無効化
+  images: {
+    // これを true にしないと、/_next/image を叩きに行って 404 になります
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
